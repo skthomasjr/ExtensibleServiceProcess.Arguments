731f74b17c9d5ebd5cde71efd2cca1e1cd27739f
Scott Keith Thomas, Jr <sthomas@idis.com>
2016-06-16 14:45:37 -0600

    Added Argument for Installing a service application.

--- Source/ExtensibleServiceProcess.CommandArguments/InstallServiceArgument.cs
+++ Source/ExtensibleServiceProcess.CommandArguments/InstallServiceArgument.cs
@@ -10,10 +10,20 @@ namespace ExtensibleServiceProcess.CommandArguments
     public class InstallServiceArgument : IArgument
     {
         /// <summary>
+        /// Initializes a new instance of the <see cref="InstallServiceArgument"/> class.
+        /// </summary>
+        /// <param name="service">The service.</param>
+        public InstallServiceArgument(ExtensibleServiceBase service)
+        {
+            Action = InstallService;
+            Target = service;
+        }
+
+        /// <summary>
         /// The action to perform when the argument is specified.
         /// </summary>
         /// <value>The action.</value>
-        public Action<string> Action { get; set; }
+        public Action<object, string> Action { get; set; }
 
         /// <summary>
         /// The possible flags used to denote the argument.
@@ -22,9 +32,21 @@ namespace ExtensibleServiceProcess.CommandArguments
         public string[] Flags { get; set; } = { "i", "install" };
 
         /// <summary>
+        /// Gets or sets the argument action target.
+        /// </summary>
+        /// <value>The target.</value>
+        public object Target { get; set; }
+
+        /// <summary>
         /// Determines if additional arguments should be processed if this argument is encountered and executed.
         /// </summary>
         /// <value><c>true</c> if [terminate after execution]; otherwise, <c>false</c>.</value>
         public bool TerminateAfterExecution { get; set; } = true;
+
+        private void InstallService(object target, string parameter)
+        {
+            var service = target as ExtensibleServiceBase;
+            // Do some work here.
+        }
     }
 }
