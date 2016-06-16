87607e207da09513d4785a63690d7628baaf6880
Scott Keith Thomas, Jr <sthomas@idis.com>
2016-06-16 12:29:21 -0600

    Initial source commit.

---
+++ Source/ExtensibleServiceProcess.CommandArguments/InstallServiceArgument.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CommandArguments;
+
+namespace ExtensibleServiceProcess.CommandArguments
+{
+    /// <summary>
+    /// Class InstallServiceArgument.
+    /// </summary>
+    /// <seealso cref="IArgument" />
+    public class InstallServiceArgument : IArgument
+    {
+        /// <summary>
+        /// The action to perform when the argument is specified.
+        /// </summary>
+        /// <value>The action.</value>
+        public Action<string> Action { get; set; }
+
+        /// <summary>
+        /// The possible flags used to denote the argument.
+        /// </summary>
+        /// <value>The flags.</value>
+        public string[] Flags { get; set; } = { "i", "install" };
+
+        /// <summary>
+        /// Determines if additional arguments should be processed if this argument is encountered and executed.
+        /// </summary>
+        /// <value><c>true</c> if [terminate after execution]; otherwise, <c>false</c>.</value>
+        public bool TerminateAfterExecution { get; set; } = true;
+    }
+}
---
+++ Source/ExtensibleServiceProcess.CommandArguments/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExtensibleServiceProcess.CommandArguments")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExtensibleServiceProcess.CommandArguments")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("086f9fd1-0a9a-4118-8066-51423e4e8ed4")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
